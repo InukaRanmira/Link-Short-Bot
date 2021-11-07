cdd43dca6f3e5ff13d25c8e2b1f54707cac0914c
Inuka Ranmira <88465820+InukaRanmira@users.noreply.github.com>
2021-11-07 16:17:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Link-Short-Bot
+# Link Short Bot
+Short link generator Bot 
+
+<p align="center"><a href="https://t.me/szteambots"><img src="https://telegra.ph/file/280da730f31e8eb4ed5a6.jpg" width="400"></a></p>
+
+<p align="center">
+
+
+
+
+## Configs
+
+* TOKEN  - Get bot token from @BotFather
+
+* API_ID     - From my.telegram.org 
+
+* API_HASH    - From my.telegram.org
+
+* BITLY_TOKEN  - From https://bitly.is/accesstoken
+
+## Deploy
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/InukaRanmira/Link-Short-Bot)
+
+## Support 🇱🇰
+
+<a href="https://t.me/szteambots"><img src="https://img.shields.io/badge/Channel%20Support%3F-yes-green?&style=flat-square?&logo=telegram" width=220px></a></p>
+
+   <a href="https://t.me/slbotzone"><img src="https://img.shields.io/badge/Group%20Support%3F-yes-green?&style=flat-square?&logo=telegram" width=220px></a></p>
+
+## Credits
+
+[Inuka Ranmira](https://github.com/InukaRanmira)
+
+<a href="https://github.com/InukaRanmira"><img src="https://telegra.ph/file/649e4231effb26e3c00f0.jpg" width="400"></a></p>
+
+
+
+
---
+++ bot.py
@@ -0,0 +1,41 @@
+
+
+from pyrogram import Client, filters
+import requests 
+import json 
+import os
+
+TOKEN = os.environ.get("TOKEN", "")
+API_ID = int(os.environ.get("API_ID",12345))
+API_HASH = os.environ.get("API_HASH","")
+BITLY_TOKEN = os.environ.get("BITLY_TOKEN","")
+
+headers = {
+    'Authorization': BITLY_TOKEN,
+    'Content-Type': 'application/json',
+}
+
+
+app = Client("bitlybot" ,bot_token = TOKEN ,api_id = API_ID ,api_hash = API_HASH )
+
+@app.on_message(filters.private & filters.command(['start']))
+async def start(client,message):
+  await message.reply_sticker(sticker = "CAACAgUAAxkBAAEDVKxhh6qgng7mU5fdxwZTyIEvi2_d7gAC1AQAAi4FOVSJf_SZU2UV7yIE")
+  await message.reply_text(f"Hello {message .from_user.first_name}\nhello i am SZ short link genrator\n made by @InukaRanmira ", reply_to_message_id = message.message_id)
+  
+@app.on_message(filters.private & filters.regex("http|https"))
+async def Bitly(client,message):
+  URL = message.text
+  DOMAIN = "bit.ly"
+  value  = {'long_url': URL , 'domain': DOMAIN}
+  data = json.dumps(value)
+  try:
+    r = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers,data = data )
+    result = r.json()
+    link = result["link"]
+    await message.reply_text(f"```{link}```", reply_to_message_id= message.message_id)
+  except Exception as e :
+    await message.reply_text(e)
+    
+app.run()
+    
